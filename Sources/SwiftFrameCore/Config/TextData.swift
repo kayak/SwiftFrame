a98576fb45f3f259f000d72dd208f78bd627ddcf
Henrik Panhans <hpanhans@kayak.com>
2020-01-27 10:29:08 +0100

    moved extension to unify files

--- Sources/SwiftFrameCore/Config/TextData.swift
+++ Sources/SwiftFrameCore/Config/TextData.swift
@@ -119,25 +119,3 @@ public struct TextData: Decodable, ConfigValidatable {
         }
     }
 }
-
-extension NSTextAlignment: Codable {
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        let alignmentString = try container.decode(String.self)
-        switch alignmentString {
-        case "left":
-            self.init(rawValue: 0)!
-        case "right":
-            self.init(rawValue: 1)!
-        case "center":
-            self.init(rawValue: 2)!
-        case "justify":
-            self.init(rawValue: 3)!
-        case "natural":
-            self.init(rawValue: 4)!
-        default:
-            throw NSError(description: "Invalid text alignment \"\(alignmentString)\" was parsed")
-        }
-    }
-
-}
--- Sources/SwiftFrameCore/Extensions/NSTextAlignment+Extensions.swift
+++ Sources/SwiftFrameCore/Extensions/NSTextAlignment+Extensions.swift
@@ -1,6 +1,25 @@
 import AppKit
 
-extension NSTextAlignment {
+extension NSTextAlignment: Codable {
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let alignmentString = try container.decode(String.self)
+        switch alignmentString {
+        case "left":
+            self.init(rawValue: 0)!
+        case "right":
+            self.init(rawValue: 1)!
+        case "center":
+            self.init(rawValue: 2)!
+        case "justify":
+            self.init(rawValue: 3)!
+        case "natural":
+            self.init(rawValue: 4)!
+        default:
+            throw NSError(description: "Invalid text alignment \"\(alignmentString)\" was parsed")
+        }
+    }
 
     var cssName: String {
         switch self {
