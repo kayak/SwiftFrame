0ee9677af650add01c63e3ab6d1713f9759f3838
Henrik Panhans <henrik@panhans.dev>
2020-03-11 16:36:34 +0100

    Refactored to use new Apple package (#9)

--- Package.swift
+++ Package.swift
@@ -12,12 +12,12 @@ let package = Package(
         .executable(name: "swiftframe", targets: ["SwiftFrame"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-package-manager", .revision("swift-5.1.4-RELEASE")),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0")
     ],
     targets: [
-        .target(name: "SwiftFrame", dependencies: ["SwiftFrameCore"]),
-        .target(name: "SwiftFrameCore", dependencies: ["SPMUtility", "Yams"]),
+        .target(name: "SwiftFrame", dependencies: ["SwiftFrameCore", "ArgumentParser"]),
+        .target(name: "SwiftFrameCore", dependencies: ["Yams"]),
         .testTarget(name: "SwiftFrameTests", dependencies: ["SwiftFrameCore"])
     ]
 )
