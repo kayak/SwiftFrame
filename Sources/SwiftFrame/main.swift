910d24adbf9ced68564dba6144973981b8387d29
Henrik Panhans <henrik@panhans.dev>
2020-07-02 09:38:18 +0200

    Updated ArgumentParser and fixed failing command parsing (#12)

--- Sources/SwiftFrame/main.swift
+++ Sources/SwiftFrame/main.swift
@@ -8,18 +8,28 @@ import ArgumentParser
 struct SwiftFrame: ParsableCommand {
 
     @Flag(help: "Prints additional information and lets you verify the config file before rendering")
-    var verbose: Bool
+    var verbose = false
 
     @Option(name: .shortAndLong, help: "Read configuration values from the specified file")
     var configPath: String
 
+    func run() throws {
+        runWrapped()
+    }
+
+    private func runWrapped() {
+        do {
+            let processor = try ConfigProcessor(filePath: configPath, verbose: verbose)
+            try processor.validate()
+            try processor.run()
+        } catch let error as NSError {
+            print(CommandLineFormatter.formatError(error.localizedDescription))
+            error.expectation.flatMap { print(CommandLineFormatter.formatWarning(title: "Expectation", text: $0)) }
+            error.actualValue.flatMap { print(CommandLineFormatter.formatWarning(title: "Actual", text: $0)) }
+            Darwin.exit(Int32(error.code))
+        }
+    }
+
 }
 
-// We need this small wrapper to retain out nicely formatted error logging
-ky_executeOrExit {
-    let instance = try SwiftFrame.parse()
-
-    let processor = try ConfigProcessor(filePath: instance.configPath, verbose: instance.verbose)
-    try processor.validate()
-    try processor.run()
-}
+SwiftFrame.main()
