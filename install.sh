9479b990e2eda4f664562b5f9b28a9300878427e
Henrik Panhans <hpanhans@kayak.com>
2020-07-15 11:43:31 +0200

    Made install script more directory safe

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-SCRIPTPATH="$(dirname "$0")"
-CURRENT="$(pwd)"
+set -e
 
-cd $SCRIPTPATH
-swift build -c release
-cp -f .build/release/SwiftFrame /usr/local/bin/swiftframe
-rm -r .build/release
+SCRIPTPATH="$(dirname "$0")"
+
+swift build -c release --package-path "$SCRIPTPATH"
+cp -f "$SCRIPTPATH/.build/release/SwiftFrame" "/usr/local/bin/swiftframe"
+rm -r "$SCRIPTPATH/.build/release"
 
 echo "Installed swiftframe into /usr/local/bin/swiftframe"
-
-cd $CURRENT
