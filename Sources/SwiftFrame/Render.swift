de3bfff79c6f6a6c405e404f25096edc8e3d41a5
Henrik Panhans <henrik@panhans.dev>
2021-12-13 11:43:58 +0100

    Scaffolding

---
+++ Sources/SwiftFrame/Render.swift
@@ -0,0 +1,36 @@
+import ArgumentParser
+import Foundation
+import SwiftFrameCore
+
+struct Render: ParsableCommand {
+
+	@Argument(help: "Read configuration values from the specified file", completion: .list(["config", "json", "yml", "yaml"]))
+	var configFilePath: String
+
+	@Flag(name: .shortAndLong, help: "Prints additional information and lets you verify the config file before rendering")
+	var verbose = false
+
+	@Flag(name: .long)
+	var noManualValidation = false
+
+	@Flag(name: .long)
+	var noColorOutput = false
+
+	func run() throws {
+		let configFileURL = URL(fileURLWithPath: configFilePath)
+
+		do {
+			let processor = try ConfigProcessor(
+				configURL: configFileURL,
+				verbose: verbose,
+				noManualValidation: noManualValidation,
+				noColorOutput: noColorOutput
+			)
+			try processor.validate()
+			try processor.run()
+		} catch let error {
+			ky_exitWithError(error, verbose: verbose)
+		}
+	}
+
+}
--- Sources/SwiftFrame/main.swift
+++ Sources/SwiftFrame/main.swift
@@ -8,37 +8,10 @@ struct SwiftFrame: ParsableCommand {
         commandName: "swiftframe",
         abstract: "CLI application for speedy screenshot framing",
         version: "4.0.1",
+		subcommands: [Render.self, Scaffold.self],
+		defaultSubcommand: Render.self,
         helpNames: .shortAndLong
     )
-
-    @Argument(help: "Read configuration values from the specified file", completion: .list(["config", "json", "yml", "yaml"]))
-    var configFilePath: String
-
-    @Flag(name: .shortAndLong, help: "Prints additional information and lets you verify the config file before rendering")
-    var verbose = false
-
-    @Flag(name: .long)
-    var noManualValidation = false
-
-    @Flag(name: .long)
-    var noColorOutput = false
-
-    func run() throws {
-        let configFileURL = URL(fileURLWithPath: configFilePath)
-
-        do {
-            let processor = try ConfigProcessor(
-                configURL: configFileURL,
-                verbose: verbose,
-                noManualValidation: noManualValidation,
-                noColorOutput: noColorOutput
-            )
-            try processor.validate()
-            try processor.run()
-        } catch let error {
-            ky_exitWithError(error, verbose: verbose)
-        }
-    }
 
 }
 
