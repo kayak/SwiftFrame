003746cbc894a0d035d342917c86e75c26bdd49e
Henrik Panhans <hpanhans@kayak.com>
2022-09-28 16:16:36 +0200

    bump version number
    Signed-off-by: Henrik Panhans <hpanhans@kayak.com>

--- Sources/SwiftFrame/SwiftFrame.swift
+++ Sources/SwiftFrame/SwiftFrame.swift
@@ -8,7 +8,7 @@ struct SwiftFrame: ParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "swiftframe",
         abstract: "CLI application for speedy screenshot framing",
-        version: "4.2.0",
+        version: "5.0.0",
         subcommands: [Render.self, Scaffold.self],
         defaultSubcommand: Render.self,
         helpNames: .shortAndLong
